6a34225c4b5e42dbff7be4860f7ae6767e775c1e
Eien Yuki <midorin452@gmail.com>
2019-03-05 21:06:56 +0800

    新增內容

--- src-site/src/pages/Project.vue
+++ src-site/src/pages/Project.vue
@@ -23,10 +23,22 @@
 			return {
 				items: [
 					{
+						img: 'project/小程式合集.png',
+						url: 'https://drive.google.com/open?id=0B2fFiOOFaSZudVhmZkxUNEZ6b00',
+						text: '小程式合集',
+						msg: '一些年代久遠的東西惹 加減看ㄅ'
+					},
+					{
 						img: 'project/Best_Predict.jpg',
 						url: 'https://http-172-17-11-0-3000.itsrv.work/home',
 						text: 'Best Predict',
 						msg: '這是學校的畢業專題 一共有5人'
+					},
+					{
+						img: 'project/臺灣學術倫理教育資源中心-自動作答程式.png',
+						url: 'https://github.com/EienYuki/ethics-p.moe.edu.tw_autoAnswer',
+						text: '自動作答程式',
+						msg: '這是一隻針對 臺灣學術倫理教育資源中心 的自動作答程式 當然答案你要自己蒐集喔！'
 					}
 				]
 			}
