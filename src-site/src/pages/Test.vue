8bdb4d44b548af061b96da7c59fdb42016aa86e8
Eien Yuki <midorin452@gmail.com>
2019-03-06 23:58:50 +0800

    test

--- src-site/src/pages/Test.vue
+++ src-site/src/pages/Test.vue
@@ -1,30 +1,13 @@
 <template lang="pug">
 	div(class="layout")
-		header(class="row header")
-			div(class="box")
-				div(class="title")
-					h1 ゆきふわり
-				div(class="icons")
-					a(href="https://t.co/y4vEqPiXW9" target="_blank" rel="noopener")
-						img(src="/icons/pixiv_icon.png", alt="pixiv_icon.png")
-					a(href="https://twitter.com/yuki_01957" target="_blank" rel="noopener")
-						img(src="/icons/Twitter_Logo_WhiteOnBlue.png", alt="GitHub-Mark-32px.png")
-					a(href="https://github.com/EienYuki" target="_blank" rel="noopener")
-						img(src="/icons/GitHub-Mark-Light-32px.png", alt="GitHub-Mark-32px.png")
-			nav
-				ul(class="container clearfix")
-					li
-						g-link(class="about" :to="{name: 'about'}") About
-					li
-						g-link(class="gallery" :to="{name: 'gallery'}") Gallery
-					li
-						g-link(class="project" :to="{name: 'project'}") Project
-					li
-						g-link(class="test" :to="{name: 'test'}") Test
-			div(class="bg")
 			
 		div(class="container view")
 			
+				
+		footer
+			div(class="text")
+				p Copyright © 2019 by EienYuki.
+			div(class="bg")
 </template>
 
 
