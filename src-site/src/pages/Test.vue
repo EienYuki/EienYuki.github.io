1883a31b8bc5fb605512dbe13786e94ff5e05134
Eien Yuki <midorin452@gmail.com>
2019-03-07 00:17:50 +0800

    test

--- src-site/src/pages/Test.vue
+++ src-site/src/pages/Test.vue
@@ -1,57 +1,46 @@
-<template>
-	<div class="container view">
-		<div class="row item">
-			<a class="col-md-3" v-for="(item, index) in items" :key="index" :href="item.url" target="_blank" rel="noopener">
-				<g-image :src="require(`!!assets-loader!~/asstes/${item.img}`)" blur="200" :alt="item.msg" />
-				<p>{{item.text}}</p>
-				<p class="msg">{{item.msg}}</p>
-			</a>
-		</div>
-	</div>
+<template lang="pug">
+	div(class="layout")
+		header(class="row header")
+			div(class="box")
+				div(class="title")
+					h1 ゆきふわり
+				div(class="icons")
+					a(href="https://t.co/y4vEqPiXW9" target="_blank" rel="noopener")
+						img(src="/icons/pixiv_icon.png", alt="pixiv_icon.png")
+					a(href="https://twitter.com/yuki_01957" target="_blank" rel="noopener")
+						img(src="/icons/Twitter_Logo_WhiteOnBlue.png", alt="GitHub-Mark-32px.png")
+					a(href="https://github.com/EienYuki" target="_blank" rel="noopener")
+						img(src="/icons/GitHub-Mark-Light-32px.png", alt="GitHub-Mark-32px.png")
+			nav
+				ul(class="container clearfix")
+					li
+						g-link(class="about" :to="{name: 'about'}") About
+					li
+						g-link(class="gallery" :to="{name: 'gallery'}") Gallery
+					li
+						g-link(class="project" :to="{name: 'project'}") Project
+					li
+						g-link(class="test" :to="{name: 'test'}") Test
+			
+		div(class="container view")
+			p 123
+				
+		footer
+			div(class="text")
+				p Copyright © 2019 by EienYuki.
+			div(class="bg")
+		
 </template>
 
 <script>
-	import myMeta from '~/mixin/meta.js'
-	
 	export default {
 		metaInfo: {
-			title: '我的專案',
-			meta: []
-		},
-		mixins: [myMeta],
-		data () {
-			return {
-				items: [
-					{
-						img: 'project/小程式合集.png',
-						url: 'https://drive.google.com/open?id=0B2fFiOOFaSZudVhmZkxUNEZ6b00',
-						text: '小程式合集',
-						msg: '一些年代久遠的東西惹 加減看ㄅ'
-					},
-					{
-						img: 'project/Best_Predict.jpg',
-						url: 'https://http-172-17-11-0-3000.itsrv.work/home',
-						text: 'Best Predict',
-						msg: '這是學校的畢業專題 一共有5人'
-					},
-					{
-						img: 'project/臺灣學術倫理教育資源中心-自動作答程式.png',
-						url: 'https://github.com/EienYuki/ethics-p.moe.edu.tw_autoAnswer',
-						text: '自動作答程式',
-						msg: '這是一隻針對 臺灣學術倫理教育資源中心 的自動作答程式 當然答案你要自己蒐集喔！'
-					}
-				]
-			}
-		},
-		created () {
-			this.$options.metaInfo.meta = [...this.$options.metaInfo.meta, ...this.generateMeta({
-				title: '我的專案',
-				description: '一些我參與過的專案',
-				image: require(`!!assets-loader!~/asstes/about/練習56-5.jpg`).src
-			})]
+			meta: [
+				{ key: 'theme-color', name: 'theme-color', content: '#ffc966' }
+			]
 		},
 		mounted () {
-			document.querySelector('nav ul a.project').classList.add('current')
-		}
+			
+		},
 	}
 </script>
